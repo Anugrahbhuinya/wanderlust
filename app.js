d8b8560d7a196b3dcae41e9835a9091201d3f5c5
Anugrahbhuinya <bhuinyaanugrah403@gmail.com>
2024-12-18 18:56:47 +0530

    added edit and delete

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();
 const mongoose  =require("mongoose");
 const Listing= require("./models/listing.js");
 const path = require("path");
+const methodOverride= require("method-override");
+const { method } = require("method");
 
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
 main().then((res)=>{console.log("result")}).catch((err)=>{console.log("error")});
@@ -57,6 +60,20 @@ app.get("/listings/:id/edit",async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
+});
+
+app.put("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+
+});
+
+app.delete("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    let deletedListing= await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings")
 })
 
 app.listen(8080, ()=>{
