eb81dec0d1ebf2160b5d74f14e39d227c4422de8
Anugrahbhuinya <bhuinyaanugrah403@gmail.com>
2024-12-30 23:17:22 +0530

    added authorization

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data=initData.data.map((obj)=>({...obj,owner:"67714d99a6b4fde275482298"}))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
